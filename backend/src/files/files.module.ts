4d8359ad25505ad24482a482da9e82fc8758c6cf
MutedKill <mack.curilencko2018@yandex.ru>
2025-12-20 11:13:15 +0300

    fix: import MinioModule in FilesModule

--- backend/src/files/files.module.ts
+++ backend/src/files/files.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
+import { MinioModule } from '../minio/minio.module';
 
 @Module({
   imports: [
     MulterModule.register({
       storage: memoryStorage(),
     }),
+    MinioModule,
   ],
   controllers: [FilesController],
 })
